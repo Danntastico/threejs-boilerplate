c0f51087f432d25c74bf400f37c7bc50eadf58b3
Dann <danntastico@proton.me>
2022-11-29 20:03:03 -0500

    Added a gridHelper

--- src/client/assets/scene.ts
+++ src/client/assets/scene.ts
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 
 const Scene = new THREE.Scene()
-Scene.add(new THREE.AxesHelper(0.2))
+Scene.add(new THREE.AxesHelper(2))
 
+const plane = new THREE.GridHelper(20, 10)
+Scene.add(plane)
 export default Scene
